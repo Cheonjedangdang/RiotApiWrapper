a569b7fb18b092fd7fb61394f9bad9820b9cc4a7
wkdtm <wkdtm@DESKTOP-UNATB13>
2016-10-14 15:18:42 +0900

    save

--- RiotApiWrapper/src/test/java/com/leelab/riotapi/apis/ChampionModule.java
+++ RiotApiWrapper/src/test/java/com/leelab/riotapi/apis/ChampionModule.java
@@ -1,8 +1,10 @@
 package com.leelab.riotapi.apis;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import com.leelab.riotapi.RestClient;
+
 
 public class ChampionModule extends APIModule{
 	
@@ -10,13 +12,18 @@ public class ChampionModule extends APIModule{
 		super(spec, client);
 	}
 	
-	public String getChampion(Locale locale, boolean freeToPlay){
+	public JSONArray getChampion(Locale locale, boolean freeToPlay){
 		getSpec().setVersion("v1.2");
 		String requestURL = getSpec().generate(locale)+"/champion?freeToPlay="+freeToPlay+"&api_key="+getSpec().getApi_key();
 		JSONObject obj = getClient().request(requestURL);
 		System.out.println(obj.toJSONString());
+		
+		JSONArray champions = (JSONArray)obj.get("champions");
+		for(Object ch : champions) {
+		      JSONObject champ = (JSONObject)ch;
+		}
 		//System.out.println(spec.generate(locale)+"/champion?freeToPlay="+freeToPlay+"&api_key="+spec.getApi_key());
-		return "";
+		return champions;
 	}
 
 	public String getChampionNumber(Locale locale, long id){
---
+++ RiotApiWrapper/src/test/java/com/leelab/riotapi/champion/Champion.java
@@ -0,0 +1,72 @@
+package com.leelab.riotapi.champion;
+
+public class Champion {
+	
+	private boolean rankedPlayEnabled;
+	private boolean botEnabled;
+	private boolean botMmEnabled;
+	private boolean active;
+	private boolean freeToPlay;
+	private long id;
+	
+	public Champion(boolean rankedPlayEnabled, boolean botEnabled, boolean botMmEnabled, boolean active,
+			boolean freeToPlay, long id) {
+		super();
+		this.rankedPlayEnabled = rankedPlayEnabled;
+		this.botEnabled = botEnabled;
+		this.botMmEnabled = botMmEnabled;
+		this.active = active;
+		this.freeToPlay = freeToPlay;
+		this.id = id;
+	}
+
+	public boolean isRankedPlayEnabled() {
+		return rankedPlayEnabled;
+	}
+
+	public void setRankedPlayEnabled(boolean rankedPlayEnabled) {
+		this.rankedPlayEnabled = rankedPlayEnabled;
+	}
+
+	public boolean isBotEnabled() {
+		return botEnabled;
+	}
+
+	public void setBotEnabled(boolean botEnabled) {
+		this.botEnabled = botEnabled;
+	}
+
+	public boolean isBotMmEnabled() {
+		return botMmEnabled;
+	}
+
+	public void setBotMmEnabled(boolean botMmEnabled) {
+		this.botMmEnabled = botMmEnabled;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public boolean isFreeToPlay() {
+		return freeToPlay;
+	}
+
+	public void setFreeToPlay(boolean freeToPlay) {
+		this.freeToPlay = freeToPlay;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+}
