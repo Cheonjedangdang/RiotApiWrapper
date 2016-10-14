e975ef1e01e1db7ef9657ca8b2af8236e4451366
leehyungyu <leehyungyu@DESKTOP-OD415VB>
2016-10-14 17:34:25 +0900

    commit

--- RiotApiWrapper/src/main/java/com/leelab/riotapi/service/SummonerSearchService.java
+++ RiotApiWrapper/src/main/java/com/leelab/riotapi/service/SummonerSearchService.java
@@ -1,25 +1,27 @@
 package com.leelab.riotapi.service;
 
+import java.util.ArrayList;
+
 import org.springframework.ui.Model;
 
 import com.leelab.riotapi.apis.GameModule;
 import com.leelab.riotapi.apis.Locale;
 import com.leelab.riotapi.apis.RiotApi;
 import com.leelab.riotapi.apis.SummonerModule;
+import com.leelab.riotapi.game.Game;
 import com.leelab.riotapi.summoner.Summoner;
 
 public class SummonerSearchService extends Service{
 
-	@Override
-	public void handleRequest(RiotApi api, Object... params) {
-		String summonerName = (String)params[0];
-		Model model = (Model)params[1];
-		
-		Summoner summoner = api.callModule(SummonerModule.class).getSummonerByName(Locale.KR, summonerName);
-		
-		model.addAttribute("summoner", summoner);
-		api.callModule(GameModule.class).getRecentGamesBySummonerId(Locale.KR, summoner.getId());
-
-	}
-	
+   @Override
+   public void handleRequest(RiotApi api, Object... params) {
+      String summonerName = (String)params[0];
+      Model model = (Model)params[1];
+      
+      Summoner summoner = api.callModule(SummonerModule.class).getSummonerByName(Locale.KR, summonerName);
+      ArrayList<Game> games = api.callModule(GameModule.class).getRecentGamesBySummonerId(Locale.KR, summoner.getId());
+      model.addAttribute("summoner", summoner);
+      model.addAttribute("games", games);
+   }
+   
 }
